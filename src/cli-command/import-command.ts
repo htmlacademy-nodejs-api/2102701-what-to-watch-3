7f16fe6b26a9eb994bb26acec90752278ae48319
Сергей Савельев <carrion19870317@gamil.com>
2022-12-27 21:35:15 +0300

    fix lint ДЗ 1.2 фикс 2

--- src/cli-command/import-command.ts
+++ src/cli-command/import-command.ts
@@ -9,7 +9,7 @@ export default class ImportCommand implements CliCommandInterface {
 
     try {
       fileReader.read();
-      console.log(`${chalk.green(JSON.stringify(fileReader.toArray(), null, 2))}`)
+      console.log(`${chalk.green(JSON.stringify(fileReader.toArray(), null, 2))}`);
     } catch (err) {
 
       if (!(err instanceof Error)) {
